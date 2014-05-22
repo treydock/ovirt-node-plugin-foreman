5bb3f219f6c4b0c1ce4f784faf8a23d9cb7a2b7d
Lukas Zapletal <lzap+git@redhat.com>
2014-05-22 14:06:09 +0200

    Adding WITH_GIT_BRANCH environment var - fix

--- aux/vagrant-build/build_image.sh
+++ aux/vagrant-build/build_image.sh
@@ -43,7 +43,7 @@ chmod +x /usr/bin/image-minimizer
 mkdir node-ws 2>/dev/null
 pushd node-ws
 [ -d ovirt-node-dev-utils ] || \
-  git clone --depth 1 -b ovirt_node_tools_gittag https://github.com/fabiand/ovirt-node-dev-utils.git dev-utils
+  git clone --depth 1 -b $ovirt_node_tools_gittag https://github.com/fabiand/ovirt-node-dev-utils.git dev-utils
 pushd dev-utils
 [ -d ovirt-node ] || make install-build-requirements clone-repos git-update WITH_GIT_BRANCH=$WITH_GIT_BRANCH
 grep $PLUGIN ovirt-node/recipe/common-pkgs.ks || \
