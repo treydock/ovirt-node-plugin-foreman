4b33d30afe839fadbc792da928b964ea9feb9a7b
Trey Dockendorf <treydock@tamu.edu>
2014-10-03 11:36:44 -0500

    Add missing dependencies

--- aux/vagrant-build/build_image.sh
+++ aux/vagrant-build/build_image.sh
@@ -16,7 +16,7 @@ yum -y install epel-release
 yum -y install livecd-tools appliance-tools-minimizer fedora-packager \
   python-devel rpm-build createrepo selinux-policy-doc checkpolicy \
   selinux-policy-devel autoconf automake python-mock python-lockfile \
-  python-nose git-review qemu-kvm hardlink git wget
+  python-nose pykickstart git-review qemu-kvm hardlink git wget
 
 sed -r -i 's/^(Defaults\s+requiretty)/#\1/g' /etc/sudoers
 
